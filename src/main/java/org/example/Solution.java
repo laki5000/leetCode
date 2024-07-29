96a57efd85221541e5bcffb852834b6c38072bb8
Lakatos Bence <lakatos816@gmail.com>
2024-07-29 18:37:59 +0200

    solved my first leetCode - Two Sum - https://leetcode.com/problems/two-sum/

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Solution {
+    // 1. Two Sum https://leetcode.com/problems/two-sum/
+    public int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+}
